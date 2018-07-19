0c1f20e1eab72a4f6437bf43960c4e441ed1ffab
JP Godara <1jpgodara@users.noreply.github.com>
2018-07-19 15:45:48 +0530

    Add NegativeArraySizeException in DiskBasedCache

--- library/src/com/android/volley/cache/DiskBasedCache.java
+++ library/src/com/android/volley/cache/DiskBasedCache.java
@@ -128,7 +128,11 @@ public class DiskBasedCache implements Cache {
             VolleyLog.d("%s: %s", file.getAbsolutePath(), e.toString());
             remove(key);
             return null;
-        } catch (OutOfMemoryError e) {
+        } catch (NegativeArraySizeException e) {
+            VolleyLog.d("%s: %s", file.getAbsolutePath(), e.toString());
+            remove(key);
+            return null;
+        }catch (OutOfMemoryError e) {
             VolleyLog.e("Caught OOM for %d byte image, path=%s: %s", file.length(), file.getAbsolutePath(), e.toString());
             return null;
         } finally {
